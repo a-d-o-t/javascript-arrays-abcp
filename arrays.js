af37df19442fcdc22bf35487c518f409178a18a4
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 09:43:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,3 +34,9 @@ function destructivelyRemoveElementFromBeginningOfArray(array)
   array.shift();
   return array
 }
+
+function removeElementFromBeginningOfArray(array)
+{
+  array.slice(1)
+  return array
+}
