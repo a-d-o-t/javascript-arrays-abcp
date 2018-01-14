73b4730eca2985fe41b0553d44c1442edba6227e
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 09:14:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
- // var newArray = ["amanda", "matt", "jeremy"];
   array.unshift(element);
   return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  //var newArray = ["amanda", "matt", "jeremy"];
   var newestArray = [element, ...array];
   return newestArray;
 }
