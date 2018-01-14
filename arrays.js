580af1758aa423515bbae9debe6e7eaa4791646e
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 09:09:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,11 +3,11 @@ var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"];
 function addElementToBeginningOfArray(newArray, addedElement) {
  // var newArray = ["amanda", "matt", "jeremy"];
   newArray.unshift(addedElement);
-  return newArray
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(newArray, addedElement) {
   //var newArray = ["amanda", "matt", "jeremy"];
   newestArray = [addedElement, ...newArray];
-  return newestArray
+  return newestArray;
 }
