2a758e74800788a39f0f32beb2e9eedc8887fd86
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 09:23:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"];
 
+
+function AddElementToBeginningOfArray(array, element) {
+  var newestArray = [element, ...array];
+  return newestArray;
+}
+
+
 function addElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  var newestArray = [element, ...array];
-  return newestArray;
 }
